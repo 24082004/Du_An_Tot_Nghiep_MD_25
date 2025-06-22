17b44c8847004111d16d50e6491344a38b6edc40
24082004 <loindph36082@fpl.edu.vn>
2025-06-22 14:12:09 +0700

    Web

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Pages/Dashboard';
 import Users from './Pages/Users';
